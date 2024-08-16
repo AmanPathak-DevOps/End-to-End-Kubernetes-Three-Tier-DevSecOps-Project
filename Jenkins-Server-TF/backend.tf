c08a58026c4333918bc35f258b9fafc053ff9bf0
Sajid Mohammad <mdsajid020@gmail.com>
2024-08-16 19:49:19 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "sajid-devops-bucket"
-    region         = "sp-south-1"
+    bucket         = "mdsajid-cicd-devops"
+    region         = "ap-south-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
