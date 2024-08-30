efc1abb8d141ee4bcc4a075b0f459dbef94c15eb
root <root@ip-172-31-9-227.ap-south-1.compute.internal>
2024-08-30 21:58:53 +0000

    all

--- Jenkins-Server-TF/iam-instance-profile.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_instance_profile" "instance-profile" {
-  name = "Jenkins-instance-profile"
-  role = aws_iam_role.iam-role.name
-}
--- Jenkins-Server-TF/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc-name" {}
-variable "igw-name" {}
-variable "rt-name" {}
-variable "subnet-name" {}
-variable "sg-name" {}
-variable "instance-name" {}
-variable "key-name" {}
-variable "iam-role" {}
