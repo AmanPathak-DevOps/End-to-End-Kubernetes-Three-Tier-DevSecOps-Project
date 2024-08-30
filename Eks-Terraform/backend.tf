fb7c37df6f0b07d19d6bd48361e00cb5265a08b4
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2024-08-31 00:25:45 +0100

    Update backend.tf

--- Eks-Terraform/backend.tf
+++ Eks-Terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = " three-tier.projects.2024"
     region         = "ap-south-1"
-    key            = "https://github.com/Manojsiriparthi/End-to-End-Kubernetes-Three-Tier-DevSecOps-Project.git/Eks-Terraform/terraform.tfstate"
+    key            = "terraform/state"
   }
   required_version = ">=0.13.0"
   required_providers {
