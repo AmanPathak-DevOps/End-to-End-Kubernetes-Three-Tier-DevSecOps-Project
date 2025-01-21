16a5dda1e31e75e64b42fc09352a56fdd7166540
Ashish Gangaramani <ashishgangaramani@gmail.com>
2025-01-20 21:10:28 -0500

    Frontend changes

--- Application-Code/frontend/src/App.js
+++ Application-Code/frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Tasks {
         return (
             <div className="app">
                 <header className="app-header">
-                    <h1>My To-Do List</h1>
+                    <h1>Ashish's To-Do List</h1>
                 </header>
                 <div className="main-content">
                     <Paper elevation={3} className="todo-container">
